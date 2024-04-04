c4277f4b7d84a2318191c65547b1bb9cdf5df5d2
Waqas Ahmad <waqasahmedwikki15@gmail.com>
2024-04-04 19:20:27 +0500

    scroll work properly

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // js begin
+let indicator = document.querySelector(".scroll-indicator .progess");
+// console.log(indicator);
+let scrollHeight =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+// console.log(scrollHeight);
+window.addEventListener("scroll", scroll);
+function scroll() {
+  let scrollTop = document.documentElement.scrollTop;
+  let scrolled = (scrollTop / scrollHeight) * 100;
+  indicator.style.width = `${scrolled}%`;
+}
